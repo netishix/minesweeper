755744b7a07849c6980fd4e82f9e8f0421b95aa7
netishix <netishix@gmail.com>
2021-03-16 21:21:33 -0300

    feat(TimeSpentPipe): Add new pipe TimeSpent

---
+++ src/app/pipes/time-spent.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimeSpentPipe } from './time-spent.pipe';
+
+describe('TimeSpentPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimeSpentPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
